7460535884e02b5f7e17e447377f467ee9b198cd
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2014-11-07 09:41:50 -0800

    Up to date ask and reporter actor

--- src/main/java/com/vmware/AskActor.java
+++ src/main/java/com/vmware/AskActor.java
@@ -5,6 +5,9 @@ import akka.actor.UntypedActor;
 public class AskActor extends UntypedActor {
     @Override
     public void onReceive(Object message) throws Exception {
-
+        if (message.equals("How do you feel today?")) {
+           Thread.sleep(10000);
+           getSender().tell("Fine", getSelf());
+        }
     }
 }
--- src/main/java/com/vmware/ReporterActor.java
+++ src/main/java/com/vmware/ReporterActor.java
@@ -3,27 +3,30 @@ package com.vmware;
 import akka.actor.ActorSelection;
 import akka.actor.UntypedActor;
 import akka.dispatch.OnSuccess;
+import akka.event.Logging;
+import akka.event.LoggingAdapter;
 import scala.concurrent.Future;
 
 import static akka.pattern.Patterns.ask;
 
 public class ReporterActor extends UntypedActor {
+
+    LoggingAdapter log = Logging.getLogger(context().system(), this);
+
     @Override
     public void onReceive(Object message) throws Exception {
        if (message.equals("go")) {
-
            ActorSelection selection =
                    context().actorSelection("/user/askactor");
-           Future<Object> future = ask(selection, "How do you feel today?", 9000);
+           Future<Object> future = ask(selection,
+                   "How do you feel today?", 15000);
+           log.debug("If we see this under 5 sec. it does not block");
            future.onSuccess(new OnSuccess<Object>() {
                @Override
                public void onSuccess(Object result) throws Throwable {
-
+                    log.info("Received Result: {}", result);
                }
            }, context().dispatcher());
-x
-
-
        }
     }
 }
